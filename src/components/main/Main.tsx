fcf8345eb9e529b7e9a9859d00bb225c45cbca77
Evgeniy Kalimullin <counsil@mail.ru>
2022-11-10 12:31:49 +0700

    applied other filters

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useRef } from 'react'
 import { useTypedDispatch } from '../../hooks/useTypedDispatch'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
 import { figuresActionCreator } from '../../store/reducers/figures/action-creators'
+import { EFigureColor, EFigureForm, IFigure } from '../../types'
 import Figure from '../figure/Figure'
 import styles from './main.module.scss'
 
@@ -9,7 +10,16 @@ const Main: React.FC = () => {
   const dispatch = useTypedDispatch()
   const divBlock = useRef<HTMLDivElement | null>(null)
   const { figures } = useTypedSelector((state) => state.figuresReducer)
-  const { columns } = useTypedSelector((state) => state.filtersReducer)
+  const {
+    isCircleVisible,
+    isSquareVisible,
+    isBlueVisible,
+    isGreenVisible,
+    isRedVisible,
+    isYellowVisible,
+    isDarkVisible,
+    columns
+  } = useTypedSelector((state) => state.filtersReducer)
 
   useEffect(() => {
     dispatch(figuresActionCreator.fetchFigures())
@@ -19,9 +29,22 @@ const Main: React.FC = () => {
     return Math.floor((divBlock.current?.getBoundingClientRect().width || 0) / columns)
   }, [columns])
 
+  const getFigures = () => {
+    return figures.filter(
+      (figure) =>
+        (!isCircleVisible ? figure.form === EFigureForm.square : true) &&
+        (!isSquareVisible ? figure.form === EFigureForm.circle : true) &&
+        (!isBlueVisible ? figure.color !== EFigureColor.blue : true) &&
+        (!isGreenVisible ? figure.color !== EFigureColor.green : true) &&
+        (!isRedVisible ? figure.color !== EFigureColor.red : true) &&
+        (!isYellowVisible ? figure.color !== EFigureColor.yellow : true) &&
+        (isDarkVisible !== null ? (isDarkVisible ? figure.dark : !figure.dark) : true)
+    )
+  }
+
   return (
     <div className={styles.main} ref={divBlock}>
-      {figures.map((fig, idx) => (
+      {getFigures().map((fig, idx) => (
         <Figure form={fig.form} color={fig.color} dark={fig.dark} width={calcFigureWidth()} key={idx} />
       ))}
     </div>
